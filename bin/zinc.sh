d87e1461381aa74a61c82cfdcfc30aa761bb988c
Stu Hood <stuhood@twitter.com>
2015-04-16 18:29:55 -0700

    Add simple runner script

---
+++ bin/zinc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+
+# zinc install
+ZINC_TGZ=$(ls -1 ${ROOT}/target/universal/*.tgz | tail -n 1)
+TMP="/tmp/zinc-tgz-`date +%s`"
+mkdir -p $TMP
+(cd $TMP && tar xzf $ZINC_TGZ)
+
+# execute
+$TMP/*/bin/zinc $*
