125b33623956f97a92ea094c3b0915df231a3cd2
Stu Hood <stuhood@twitter.com>
2015-04-20 08:34:29 -0700

    Drop to initial deps

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -6,14 +6,16 @@ import sbt._
 import sbt.Keys._
 
 object ZincBuild extends Build {
-  val vSuffix = 13
-  val sbtVersion = s"0.13.7-JAR-OUTPUT-${vSuffix}"
+  val sbtSuffix = 15
+  val zincSuffix = 18
+
+  val sbtVersion = s"0.13.7-JAR-OUTPUT-${sbtSuffix}"
 
   val resolveSbtLocally = settingKey[Boolean]("resolve-sbt-locally")
 
   lazy val buildSettings = Seq(
     organization := "com.typesafe.zinc",
-    version := s"0.3.7-JAR-OUTPUT-${vSuffix}",
+    version := s"0.3.7-JAR-OUTPUT-${sbtSuffix}-${zincSuffix}",
     scalaVersion := "2.10.4",
     crossPaths := false
   )
@@ -25,10 +27,8 @@ object ZincBuild extends Build {
       resolveSbtLocally := true,
       resolvers += (if (resolveSbtLocally.value) Resolver.mavenLocal else Opts.resolver.sonatypeSnapshots),
       libraryDependencies ++= Seq(
-        "org.scala-sbt" % "compile" % sbtVersion,
-        "org.scala-sbt" % "compiler-integration" % sbtVersion,
-        "org.scala-sbt" % "compiler-interface" % sbtVersion classifier "src",
         "org.scala-sbt" % "incremental-compiler" % sbtVersion,
+        "org.scala-sbt" % "compiler-interface" % sbtVersion classifier "sources",
         "com.martiansoftware" % "nailgun-server" % "0.9.1" % "optional"
       ),
       scalacOptions ++= Seq("-feature", "-deprecation", "-Xlint")
