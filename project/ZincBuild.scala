44a6dcb8f50d156b8a40b30700034aa1a90ebecf
Stu Hood <stuhood@twitter.com>
2015-05-03 11:04:36 -0700

    Final (?) bump.

--- project/ZincBuild.scala
+++ project/ZincBuild.scala
@@ -12,7 +12,7 @@ object ZincBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "com.typesafe.zinc",
-    version := "0.3.7-stuhood-no-analysis-hash-3",
+    version := "0.3.7-stuhood-no-analysis-hash-4",
     scalaVersion := "2.10.4",
     crossPaths := false
   )
